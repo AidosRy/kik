febb793ef9f8abc61902c7bb9e167b5690a80aae
aidosr <qaidosrustemp@gmail.com>
2020-09-13 20:08:25 +0300

    initialization

---
+++ src/main/java/com/kik/KazItalKozhaApplication.java
@@ -0,0 +1,13 @@
+package com.kik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KazItalKozhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KazItalKozhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kik/KazItalKozhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KazItalKozhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
