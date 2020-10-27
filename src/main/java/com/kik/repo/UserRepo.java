6c494afc1b439835185119ec650e3dc1ccc88a81
aidosr <qaidosp@gmail.com>
2020-10-27 17:11:38 +0600

    find by email

--- src/main/java/com/kik/repo/UserRepo.java
+++ src/main/java/com/kik/repo/UserRepo.java
@@ -3,5 +3,8 @@ package com.kik.repo;
 import com.kik.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
